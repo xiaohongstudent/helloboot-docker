7ac708589eacb25c0c768dd2b247a54c99ec8f9c
taolong.hong <taolong.hong@ubtrobot.com>
2020-04-07 14:59:25 +0800

    首次提交

---
+++ src/test/java/com/taolong/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taolong.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
